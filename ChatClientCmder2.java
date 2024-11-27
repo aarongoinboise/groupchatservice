1f5ee1a87f584d0ff6668907a0b5ff87bf627988
aarongoinboise <aarongoin@u.boisestate.edu>
2024-11-27 12:04:38 -0700

    simpler imp

--- ChatClientCmder2.java
+++ ChatClientCmder2.java
@@ -8,103 +8,73 @@ import java.util.concurrent.Executors;
 
 public class ChatClientCmder2 {
     public static ExecutorService pool;
-    private boolean connected;
-
-    public ChatClientCmder2() {
-
-    }
+    private static boolean connected;
     
-    public static void connect(String serverName, String pString) throws Exception {
-        char[] ps = pString.toCharArray();
-        for (char c : ps) {
-            if (!Character.isDigit(c)) {
-                throw new Exception();
+        public ChatClientCmder2() {
+            connected = false;
+        }
+        
+        public static void connect(String serverName, String pString) throws Exception {
+            char[] ps = pString.toCharArray();
+            for (char c : ps) {
+                if (!Character.isDigit(c)) {
+                    throw new Exception();
+                }
             }
+            int port = Integer.parseInt(pString);
+            pool = Executors.newSingleThreadExecutor(); 
+            Socket clientSocket;
+            ObjectOutputStream out;
+            ObjectInputStream in;
+            try {
+                clientSocket = new Socket(serverName, port);
+                out = new ObjectOutputStream(clientSocket.getOutputStream());
+                in = new ObjectInputStream(clientSocket.getInputStream());
+            } catch (IOException e) {
+                System.out.println("problem connecting to the server");
+                e.printStackTrace();
+                return;
+            }
+            System.out.println("Connected to server " + serverName + " at port " + port);
+            pool.execute(new ChatHandler(in, out));
         }
-        int port = Integer.parseInt(pString);
-        pool = Executors.newFixedThreadPool(2);
-        Socket clientSocket;
-        ObjectOutputStream out;
-        ObjectInputStream in;
-        try {
-            clientSocket = new Socket(serverName, port);
-            out = new ObjectOutputStream(clientSocket.getOutputStream());
-            in = new ObjectInputStream(clientSocket.getInputStream());
-        } catch (IOException e) {
-            System.out.println("problem connecting to the server");
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("Connected to server " + serverName + " at port " + port);
-        pool.execute(new MessageReceiver(in));
-        pool.execute(new MessageSender(out));
-    }
-
-    /**
-     * runnable class which waits for user input (a message to be sent on the chat server) and sends it to the chat server
-     */
-    private static class MessageSender implements Runnable {
-        ObjectOutputStream outputStream;
-
-        /**
-         * Constructor: sets, OOS and scanner
-         * 
-         * @param outputStream vehicle that sends messages to server
-         * @param inputScanner same scanner as in main, scans user input to send msgs to server
-         */
-        public MessageSender(ObjectOutputStream outputStream){
-            this.outputStream = outputStream;
-        }
-
-        @Override
-        public void run() {
-            while(connected){
-                scannerInServer = true;
-                userInput = inputScanner.nextLine();
+    
+        private static class ChatHandler implements Runnable {
+            private ObjectInputStream inputStream;
+            private ObjectOutputStream outputStream;
+            private Scanner inputScanner = new Scanner(System.in);
+    
+            public ChatHandler(ObjectInputStream inputStream, ObjectOutputStream outputStream) {
+                this.inputStream = inputStream;
+                this.outputStream = outputStream;
+            }
+    
+            @Override
+            public void run() {
                 try {
-                    outputStream.writeObject(new ChatMessage(userInput));
-                    outputStream.flush();
-                    if(userInput.equals(QUIT_COMMAND)){
-                        connected = false;
+                    while (connected) {
+                    // Handle receiving messages from server
+                    if (inputStream.available() > 0) {
+                        ChatMessage message = (ChatMessage) inputStream.readObject();
+                        System.out.println(message.getSender() + ": " + message.getMessage());
                     }
 
-                } catch (IOException e) {
-                    connected = false;
+                    // Handle sending messages from user input
+                    if (inputScanner.hasNextLine()) {
+                        String userInput = inputScanner.nextLine();
+                        outputStream.writeObject(new ChatMessage(userInput));
+                        outputStream.flush();
+
+                        if (userInput.equalsIgnoreCase("quit")) {
+                            connected = false;
+                        }
+                    }
                 }
+            } catch (IOException | ClassNotFoundException e) {
+                connected = false;
+                System.out.println("Connection lost");
+                e.printStackTrace();
             }
         }
-
-    }
-
-    /**
-     * runnable class which waits for messages from the chat server and displays them to the user
-     */
-    private static class MessageReceiver implements Runnable {
-        ObjectInputStream inputStream;
-
-        /**
-         * Constructor: sets inputStream
-         * 
-         * @param inputStream
-         */
-        public MessageReceiver(ObjectInputStream inputStream){
-            this.inputStream = inputStream;
-        }
-
-        @Override
-        public void run() {
-            ChatMessage message;
-            while(connected){
-                try {
-                    message = (ChatMessage) inputStream.readObject();
-                    System.out.println(message.getSender() + ": " + message.getMessage());
-                } catch (ClassNotFoundException | IOException e) {
-                    connected = false;
-                    break;
-                }
-            }
-        }
-
-
     }
 }
